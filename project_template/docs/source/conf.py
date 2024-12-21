4750708adba1d7afabc491b7c4eb60ad4e54b01a
Katarzyna Rzęsikowska <katarzyna.rzesikowska@codepoets.it>
2024-12-21 15:49:30 +0100

    add sphinx

--- project_template/docs/source/conf.py
+++ project_template/docs/source/conf.py
@@ -5,7 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+import os
 
+sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
 project = 'project_template'
 copyright = '2024, Kasia Rzęsikowska'
 author = 'Kasia Rzęsikowska'
@@ -14,7 +17,7 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +27,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
---
+++ project_template/docs/source/project_template.rst
@@ -0,0 +1,21 @@
+project\_template package
+=========================
+
+Submodules
+----------
+
+project\_template.file\_with\_code module
+-----------------------------------------
+
+.. automodule:: project_template.file_with_code
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: project_template
+   :members:
+   :undoc-members:
+   :show-inheritance:
